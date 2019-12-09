2130a7104867f2e9f482c35c2b2237550e81eb15
Kass <cabacangwork@gmail.com>
2019-12-09 11:53:24 +0800

    update

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = ('mongodb+srv://recipeDB:Passw0rd3@cluster0-h1g1p.mongodb.net/test?retryWrites=true&w=majority')
+const MONGOURL = ('mongodb+srv://recipeDB:Passw0rd3@cluster0-h1g1p.mongodb.net/test?retryWrites=true&w=majority')
 
-mongoose.connect(uri, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true });
-
-const connection = mongoose.connection;
-
-connection.once('open', () => console.log('MongoDB connection established successfully'));
+mongoose.connect(MONGOURL, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected successfully!'))
+    .catch(() => console.log('Failed to Connect!'));
 
 app.use('/recipes', require('./routes/recipes'));
 
-app.listen(3000, () => console.log('Server is running on port: 3000'));
+const port = process.env.Port || 3000;
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
